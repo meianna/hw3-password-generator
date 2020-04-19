65668491e4f0dc8412dcf430235a9e20fd029af2
Meianna Oeser <meianna.oeser@gmail.com>
2020-04-19 02:50:13 -0700

    working on for loop and repositioned EventListener

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", "?", "`", "~", ""];
-var totalChars = lowerCase + upperCase + numbers + specialChars;
 
 // Write password to the #password input
-function writePassword() {
+generateBtn.addEventListener("click", function writePassword() {
+
   // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
+  var passwordContent = "";
+  var passwordGenerated = "";
 
   var setLength = prompt("Set the length of your password between 8 and 128 characters.");
   if (setLength < 8 || setLength > 128) {
@@ -20,21 +22,7 @@ function writePassword() {
   var setUppercase = confirm("Do you want your password to contain uppercase letters?");
   var setNumbers = confirm("Do you want your password to contain numbers?");
   var setSpecialChars = confirm("Do you want your password to contain special charcters?");
-  var passwordContent = "";
-  var passwordLength = document.getElementById("#password").value;
 
-  if (setLowercase === true) {
-    passwordContent += lowerCase;
-  }
-  if (setUppercase === true) {
-    passwordContent += upperCase;
-  }
-  if (setNumbers === true) {
-    passwordContent += numbers;
-  }
-  if (setSpecialChars === true) {
-    passwordContent += specialChars;
-  }
   if (
     setLowercase === false &&
     setUppercase === false &&
@@ -43,15 +31,46 @@ function writePassword() {
   ) {
     alert("You must select at least one password criterion.")
   };
+  if (setLowercase === true) {
+    passwordContent += lowerCase;
+  };
+  if (setUppercase === true) {
+    passwordContent += upperCase;
+  };
+  if (setNumbers === true) {
+    passwordContent += numbers;
+  };
+  if (setSpecialChars === true) {
+    passwordContent += specialChars;
+  };
 
+  for (i = 0; i < setLength; i++) {
+    passwordGenerated += passwordContent.charAt(
+      Math.floor(Math.random() * passwordContent.length)
+    )
+
+    return passwordGenerated;
+
+
+    // var randomChar = Math.floor(Math.random() * passwordContent.length);
+    // password += passwordContent[randomChar];
+  }
 
 
   passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+});
+
+
+
 
 
 
