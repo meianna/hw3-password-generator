22adc3cdf6ed73ff8af25b32db05a05c50462bfc
Meianna Oeser <meianna.oeser@gmail.com>
2020-04-19 00:55:34 -0700

    added some conditionals

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", "?", "`", "~", ""];
 var totalChars = lowerCase + upperCase + numbers + specialChars;
 
-generateBtn.addEventListener("click", writePassword);
-
 // Write password to the #password input
 function writePassword() {
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
 
   var setLength = prompt("Set the length of your password between 8 and 128 characters.");
+  if (setLength < 8 || setLength > 128) {
+    prompt("Please choose a number between 8 and 128.");
+  }
+
   var setLowercase = confirm("Do you want your password to contain lowercase letters?");
   var setUppercase = confirm("Do you want your password to contain uppercase letters?");
   var setNumbers = confirm("Do you want your password to contain numbers?")
   var setSpecialChars = confirm("Do you want your password to contain special charcters?");
+
+  if (
+    setLowercase === false &&
+    setUppercase === false &&
+    setNumbers === false &&
+    setSpecialChars === false
+  ) {
+    alert("You must select at least one password criterion.")
+  }
+
 
 
   passwordText.value = password;
